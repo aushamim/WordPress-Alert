ea5621d93a8273f50741a6beff09517b44c8080f
aushamim <au.shamim0@gmail.com>
2024-09-25 11:30:04 +0600

    [Fix] Select width

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '55673c0cca8a4db9a525');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '6afb24318d3ed1ca7fea');
--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => 'e692a68a9dcc20c42d71', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => 'b3bf6b9799c37e63b2e1', 'type' => 'module');
